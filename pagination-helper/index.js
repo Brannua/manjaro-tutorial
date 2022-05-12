8bf67cf0f2696b1ae8ca40ece3a1c7f01840c1b9
Brannua <374453156@qq.com>
2022-05-12 18:17:04 +0800

    tested pagination-helper

--- pagination-helper/index.js
+++
@@ -1,7 +0,0 @@
-
-import { linkRefresher } from './src.js';
-
-window.$docsify = window.$docsify || {};
-window.$docsify.plugins = (window.$docsify.plugins || []).concat([
-  (hook, vm) => hook.doneEach(linkRefresher)
-]);
